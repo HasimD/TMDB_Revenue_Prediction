4987974ddefd7badb0296432e8fa2afc77dce4cc
HasimD <delibasahmet@gmail.com>
2019-03-21 04:33:24 +0300

    first model

--- TmdbBoxOffice_Modelling.py
+++ TmdbBoxOffice_Modelling.py
@@ -8,4 +8,75 @@ from sklearn.model_selection import KFold
 
 train = pd.read_csv("train_features.csv") 
 test = pd.read_csv("test_features.csv")
+train.fillna(value=0.0, inplace = True)
+test.fillna(value=0.0, inplace = True)
+train['revenue'] = np.log1p(train['revenue'])
+y = train['revenue'].values
+train.drop(['revenue'], axis=1,inplace=True)
 
+
+
+#------------------------- FIRST MODEL------------------------------
+
+random_seed = 2019
+k = 10
+fold = list(KFold(k, shuffle = True, random_state = random_seed).split(train))
+np.random.seed(random_seed)
+
+import xgboost as xgb
+
+def xgb_model(trn_x, trn_y, val_x, val_y, test, verbose) :
+    
+    params = {'objective': 'reg:linear', 
+              'eta': 0.01, 
+              'max_depth': 6, 
+              'subsample': 0.6, 
+              'colsample_bytree': 0.7,  
+              'eval_metric': 'rmse', 
+              'seed': random_seed, 
+              'silent': True,
+    }
+    
+    record = dict()
+    model = xgb.train(params
+                      , xgb.DMatrix(trn_x, trn_y)
+                      , 100000
+                      , [(xgb.DMatrix(trn_x, trn_y), 'train'), (xgb.DMatrix(val_x, val_y), 'valid')]
+                      , verbose_eval=verbose
+                      , early_stopping_rounds=500
+                      , callbacks = [xgb.callback.record_evaluation(record)])
+    best_idx = np.argmin(np.array(record['valid']['rmse']))
+
+    val_pred = model.predict(xgb.DMatrix(val_x), ntree_limit=model.best_ntree_limit)
+    test_pred = model.predict(xgb.DMatrix(test), ntree_limit=model.best_ntree_limit)
+
+    return {'val':val_pred, 'test':test_pred, 'error':record['valid']['rmse'][best_idx], 'importance':[i for k, i in model.get_score().items()]}
+
+result_dict = dict()
+val_pred = np.zeros(train.shape[0])
+test_pred = np.zeros(test.shape[0])
+final_err = 0
+verbose = False
+
+for i, (trn, val) in enumerate(fold) :
+    print(i+1, "fold.    RMSE")
+    
+    trn_x = train.loc[trn, :]
+    trn_y = y[trn]
+    val_x = train.loc[val, :]
+    val_y = y[val]
+    
+    fold_val_pred = []
+    fold_test_pred = []
+    fold_err = []
+    
+    #""" xgboost
+    start = datetime.now()
+    result = xgb_model(trn_x, trn_y, val_x, val_y, test, verbose)
+    fold_val_pred.append(result['val']*0.2)
+    fold_test_pred.append(result['test']*0.2)
+    fold_err.append(result['error'])
+    print("xgb model.", "{0:.5f}".format(result['error']), '(' + str(int((datetime.now()-start).seconds/60)) + 'm)')
+
+
+#------------------------- FIRST MODEL------------------------------    
